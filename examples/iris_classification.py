5debefc54bc4aaffd77881d2367e78e29273aceb
Nadav Kahlon <nadavkahlon100@gmail.com>
2024-07-22 23:26:02 +0300

    Use package-exposed MulticlassSVM

--- examples/iris_classification.py
+++ examples/iris_classification.py
@@ -12,7 +12,7 @@ sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 )
 
-from svm.multiclass_svm import MulticlassSVM
+from svm import MulticlassSVM
 from svm.plots import plot_confusion_tables, plot_confusion_mat, \
     scatter_plot_mat
 
